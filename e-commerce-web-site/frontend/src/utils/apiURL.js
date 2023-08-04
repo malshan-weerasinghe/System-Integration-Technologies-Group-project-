a96e7ab71985d5f08a643fbc8f61ad1f3d3cc095
B.A Thisara Dilshan <88025945+THISARA005@users.noreply.github.com>
2023-08-04 19:08:38 +0530

    backend folder removed and replaced.

--- e-commerce-web-site/frontend/src/utils/apiURL.js
+++
@@ -1,3 +0,0 @@
-//export const BASE_URL = `https://api.escuelajs.co/api/v1/`;
-//export const BASE_URL = `https://cors-anywhere.herokuapp.com/https://dummyjson.com/`;
-export const BASE_URL = `http://localhost:3000/proxy?url=https://dummyjson.com/`;
--- e-commerce-web-site/frontend/src/utils/helpers.js
+++
@@ -1,7 +0,0 @@
-export const formatPrice = (price) => {
-    return new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: "USD",
-        minimumFractionDigits: 2,  // force two decimal places
-    }).format(price);
-}
