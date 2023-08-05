b3bf0b207817ac05b7a6a8d2e9c5e5186c6018b2
B.A Thisara Dilshan <88025945+THISARA005@users.noreply.github.com>
2023-08-05 17:06:22 +0530

    Add the submit button.

--- e-commerce-web-site/backend/resources/views/auth/reset-password.blade.php
+++ e-commerce-web-site/backend/resources/views/auth/reset-password.blade.php
@@ -23,7 +23,7 @@
 
             <div class="mt-4">
                 <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
+                <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password " />
             </div>
 
         </form>
