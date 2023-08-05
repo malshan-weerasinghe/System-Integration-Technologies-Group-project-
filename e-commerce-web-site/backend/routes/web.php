dff8548fd1284680f859a3dc3d0c929583e20355
nshihara <nshihara99@gmail.com>
2023-08-06 00:42:05 +0530

    Add the routes that require authentication

--- e-commerce-web-site/backend/routes/web.php
+++ e-commerce-web-site/backend/routes/web.php
@@ -22,3 +22,17 @@ Route::get('/', [HomeController::class, 'checkRoleid']);
 Route::post('/admin/change-status/{user}', [Approval::class, 'switchStatus'])
     ->name('admin.approve')
     ->middleware('adminOrEmployee');
+Route::middleware(['auth'])->group(function () {
+        // Customer routes
+        Route::resource('dashboard/customer', CustomerController::class)->name('index', 'customer');
+        
+        // Employee routes
+        Route::resource('dashboard/employee', EmployeeController::class)->name('index', 'employee');
+        
+        // Approval routes
+        Route::resource('dashboard/approval', Approval::class)->name('index', 'approval');
+        
+        // View for orders
+        Route::view('dashboard/orders', 'dashboard.orders')->name('orders');
+    });
+    
