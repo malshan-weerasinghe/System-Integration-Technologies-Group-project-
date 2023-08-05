48e01086618129e01f25386b91f51a34642827f2
nshihara <nshihara99@gmail.com>
2023-08-06 01:13:18 +0530

    Testing email verification functionality

---
+++ e-commerce-web-site/backend/tests/Feature/EmailVerificationTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Auth\Events\Verified;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\URL;
+use Laravel\Fortify\Features;
+use Tests\TestCase;
+
+class EmailVerificationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_email_verification_screen_can_be_rendered(): void
+    {
+        if (! Features::enabled(Features::emailVerification())) {
+            $this->markTestSkipped('Email verification not enabled.');
+
+            return;
+        }
+
+        $user = User::factory()->withPersonalTeam()->unverified()->create();
+
+        $response = $this->actingAs($user)->get('/email/verify');
+
+        $response->assertStatus(200);
+    }
+
+   
+
+    
+}
