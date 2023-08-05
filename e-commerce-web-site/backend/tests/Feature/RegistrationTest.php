f89ae3bf3c6d944d7aa1cb040e78c46b6b8b94ba
nshihara <nshihara99@gmail.com>
2023-08-06 01:32:18 +0530

    Test cases to help ensure that the registration feature functions correctly and that users can register successfully

---
+++ e-commerce-web-site/backend/tests/Feature/RegistrationTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Fortify\Features;
+use Laravel\Jetstream\Jetstream;
+use Tests\TestCase;
+
+class RegistrationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_registration_screen_can_be_rendered(): void
+    {
+        if (! Features::enabled(Features::registration())) {
+            $this->markTestSkipped('Registration support is not enabled.');
+
+            return;
+        }
+
+        $response = $this->get('/register');
+
+        $response->assertStatus(200);
+    }
+
+ 
+
+   
+}
