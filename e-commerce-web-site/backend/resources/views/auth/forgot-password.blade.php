3751328dffcdc7cfdb4cb9859b068e8ef9bf68bd
B.A Thisara Dilshan <88025945+THISARA005@users.noreply.github.com>
2023-08-05 16:55:11 +0530

    Add password reset request form view
    This commit adds the password reset request form view to the project. The form allows users to request a password reset by providing their email address. It includes a message explaining the password reset process and displays a success message if the reset link is successfully sent. The form is styled using Tailwind CSS classes. This commit enhances the project with password reset functionality.

--- e-commerce-web-site/backend/resources/views/auth/forgot-password.blade.php
+++ e-commerce-web-site/backend/resources/views/auth/forgot-password.blade.php
@@ -16,6 +16,19 @@
 
         <x-validation-errors class="mb-4" />
 
-        
+        <form method="POST" action="{{ route('password.email') }}">
+            @csrf
+
+            <div class="block">
+                <x-label for="email" value="{{ __('Email') }}" />
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <x-button>
+                    {{ __('Email Password Reset Link') }}
+                </x-button>
+            </div>
+        </form>
     </x-authentication-card>
 </x-guest-layout>
