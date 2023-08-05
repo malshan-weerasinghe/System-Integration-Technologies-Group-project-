1d146baa07d82bbf3e781e243883f14c65641ce8
B.A Thisara Dilshan <88025945+THISARA005@users.noreply.github.com>
2023-08-05 17:04:17 +0530

    Add password reset form view

---
+++ e-commerce-web-site/backend/resources/views/auth/reset-password.blade.php
@@ -0,0 +1,14 @@
+<x-guest-layout>
+    <x-authentication-card>
+        <x-slot name="logo">
+            <x-authentication-card-logo />
+        </x-slot>
+
+        <x-validation-errors class="mb-4" />
+
+        <form method="POST" action="{{ route('password.update') }}">
+            @csrf
+
+        </form>
+    </x-authentication-card>
+</x-guest-layout>
