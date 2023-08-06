b07198c25801d1acc4759d616e0150948546b265
B.A Thisara Dilshan <88025945+THISARA005@users.noreply.github.com>
2023-08-06 09:02:03 +0530

    Add test for enabling Two-Factor Authentication
    The test case verifies that 2FA can be enabled successfully, checks that the user's two-factor secret is not null after enabling, and also verifies that the user has 8 recovery codes available. The message is concise and descriptive, making it easier for others to understand the changes when reviewing the commit history.

---
+++ e-commerce-web-site/backend/tests/Feature/TwoFactorAuthenticationSettingsTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Fortify\Features;
+use Laravel\Jetstream\Http\Livewire\TwoFactorAuthenticationForm;
+use Livewire\Livewire;
+use Tests\TestCase;
+
+class TwoFactorAuthenticationSettingsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_two_factor_authentication_can_be_enabled(): void
+    {
+        if (! Features::canManageTwoFactorAuthentication()) {
+            $this->markTestSkipped('Two factor authentication is not enabled.');
+
+            return;
+        }
+
+        $this->actingAs($user = User::factory()->create());
+
+        $this->withSession(['auth.password_confirmed_at' => time()]);
+
+        Livewire::test(TwoFactorAuthenticationForm::class)
+            ->call('enableTwoFactorAuthentication');
+
+        $user = $user->fresh();
+
+        $this->assertNotNull($user->two_factor_secret);
+        $this->assertCount(8, $user->recoveryCodes());
+    }
+
+    
+}
