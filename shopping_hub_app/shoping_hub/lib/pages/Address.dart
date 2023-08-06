e919e6ca14ebb24444bb1247dcdc3be0c1a7fbac
malshan-weerasinghe <92565486+malshan-weerasinghe@users.noreply.github.com>
2023-08-06 13:18:37 +0530

    adress controller design

--- shopping_hub_app/shoping_hub/lib/pages/Address.dart
+++ shopping_hub_app/shoping_hub/lib/pages/Address.dart
@@ -62,3 +62,263 @@ void _getLocation() async {
   }
   return null; // Return null instead of throwing an error
 }
+ @override
+  Widget build(BuildContext context) {
+    Color navbar =
+        Theme.of(context).brightness == Brightness.dark ? HexColor("#333333") : Colors.white;
+    Color topic = Theme.of(context).brightness == Brightness.dark ? HexColor("#0EC42B") : HexColor("#575353");
+    Color icon = Theme.of(context).brightness == Brightness.dark ? HexColor("#0EC42B") : HexColor("#000000");
+    Color subtext = Theme.of(context).brightness == Brightness.dark ? HexColor("#FFFFFF") : HexColor("#848484");
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(left: 45.0, right: 40.0, top: 93.0),
+              child: Row(
+                children: [
+                  GestureDetector(
+                    onTap: () => Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return const cartPage();
+                        },
+                      ),
+                    ),
+                    child: Icon(
+                      Icons.arrow_back_ios_rounded,
+                      color: topic,
+                    ),
+                  ),
+                  SizedBox(width: 10.0),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 70.0),
+                    child: Text(
+                      'Address',
+                      style: TextStyle(
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.bold,
+                        color: topic,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 5, right: 240.0, top: 24.0),
+              child: Text(
+                'Delivery',
+                style: TextStyle(fontSize: 17.0,),
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Center(
+              child: Column(
+                children: [
+                  Container(
+                    width: 300.0,
+                    height: 50.0,
+                    decoration: BoxDecoration(
+                      color: navbar,
+                      borderRadius: BorderRadius.circular(5.0),
+                      border: Border.all(color: HexColor("#848484")),
+                    ),
+                    child: TextField(
+                      controller: _addressController, // Bind the controller
+                      decoration: InputDecoration(
+                        contentPadding: const EdgeInsets.only(left: 10.0, top: -6.0),
+                        hintText: 'Address',
+                        hintStyle: TextStyle(
+                          fontSize: 15.0,
+                          color: subtext,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  Container(
+                    width: 300.0,
+                    height: 50.0,
+                    decoration: BoxDecoration(
+                      color: navbar,
+                      borderRadius: BorderRadius.circular(5.0),
+                      border: Border.all(color: HexColor("#848484")),
+                    ),
+                    child: TextField(
+                      controller: _cityController, // Bind the controller
+                      decoration: InputDecoration(
+                        contentPadding: const EdgeInsets.only(left: 10.0, top: -6.0),
+                        hintText: 'City',
+                        hintStyle: TextStyle(
+                          fontSize: 15.0,
+                          color: subtext,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  Container(
+                    width: 300.0,
+                    height: 50.0,
+                    decoration: BoxDecoration(
+                      color: navbar,
+                      borderRadius: BorderRadius.circular(5.0),
+                      border: Border.all(color: HexColor("#848484")),
+                    ),
+                    child: TextField(
+                      controller: _zipCodeController, // Bind the controller
+                      decoration: InputDecoration(
+                        contentPadding: const EdgeInsets.only(left: 10.0, top: -6.0),
+                        hintText: 'Zip Code',
+                        hintStyle: TextStyle(
+                          fontSize: 15.0,
+                          color: subtext,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  Text(
+                    'Or',
+                    style: TextStyle(fontSize: 15.0,),
+                  ),
+                  SizedBox(height: 20.0),
+                  Column(
+                    children: [
+                      Container(
+                        width: 300.0,
+                        height: 50.0,
+                        decoration: BoxDecoration(
+                          color: navbar,
+                          borderRadius: BorderRadius.circular(5.0),
+                          border: Border.all(color: HexColor("#848484")),
+                        ),
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                              child: Icon(
+                                Icons.location_on,
+                                color: icon,
+                              ),
+                            ),
+                            Expanded(
+                              child: Text(
+                                _position != null ? 'Current Location: ' + _position.toString() : 'No data',
+                                style: TextStyle(
+                                  fontSize: 15.0,
+                                  color: subtext,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                              child: GestureDetector(
+                                onTap: _getLocation,
+                                child: Icon(
+                                  Icons.edit_location,
+                                  color: icon,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 8.0),
+                      // Additional widgets...
+                    ],
+                  ),
+                  SizedBox(height: 10.0),
+                  Padding(
+                    padding: EdgeInsets.only(left: 50, right: 240.0),
+                    child: Text(
+                      'Delivery Type',
+                      style: TextStyle(fontSize: 17.0,),
+                    ),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 45.0),
+                        child: RadioListTile(
+                          activeColor: HexColor("#0EC42B"),
+                          title: Text('Urgent Delivery'),
+                          value: 'Urgent Delivery',
+                          groupValue: deliveryType,
+                          onChanged: (value) {
+                            setState(() {
+                              deliveryType = value.toString();
+                            });
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 45.0, bottom: 45.0),
+                        child: RadioListTile(
+                          activeColor: HexColor("#0EC42B"),
+                          title: Text('Normal Delivery'),
+                          value: 'Express',
+                          groupValue: deliveryType,
+                          onChanged: (value) {
+                            setState(() {
+                              deliveryType = value.toString();
+                            });
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10.0),
+                  GestureDetector(
+                    onTap: () {
+                      _saveAddress(); // Call the function to save address
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return const Preview();
+                          },
+                        ),
+                      );
+                    },
+                    child: Container(
+                      width: 275.0,
+                      height: 50.0,
+                      decoration: BoxDecoration(
+                        color: HexColor("#0EC42B"),
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                      padding: const EdgeInsets.only(left: 25.0),
+                      child: Row(
+                        children: [
+                          Text(
+                            "Preview",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              fontSize: 17.0,
+                              fontFamily: 'Roboto',
+                              color: HexColor("#FFFFFF"),
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(width: 120.0), // Add a space of 5.0 between the text and the icon
+                          Icon(
+                            Icons.arrow_forward_ios_rounded,
+                            color: HexColor("#FFFFFF"),
+                            size: 25.0,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+          ),
+          
+        ),
