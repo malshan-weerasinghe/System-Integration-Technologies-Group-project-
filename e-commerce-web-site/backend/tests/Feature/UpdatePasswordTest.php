b9f997b8247997ed013b40d5c57b100ada3528f7
B.A Thisara Dilshan <88025945+THISARA005@users.noreply.github.com>
2023-08-06 09:31:30 +0530

    Add test for correct current password validation

--- e-commerce-web-site/backend/tests/Feature/UpdatePasswordTest.php
+++ e-commerce-web-site/backend/tests/Feature/UpdatePasswordTest.php
@@ -28,5 +28,21 @@ class UpdatePasswordTest extends TestCase
         $this->assertTrue(Hash::check('new-password', $user->fresh()->password));
     }
 
+    public function test_current_password_must_be_correct(): void
+    {
+        $this->actingAs($user = User::factory()->create());
+
+        Livewire::test(UpdatePasswordForm::class)
+            ->set('state', [
+                'current_password' => 'wrong-password',
+                'password' => 'new-password',
+                'password_confirmation' => 'new-password',
+            ])
+            ->call('updatePassword')
+            ->assertHasErrors(['current_password']);
+
+        $this->assertTrue(Hash::check('password', $user->fresh()->password));
+    }
+
    
 }
